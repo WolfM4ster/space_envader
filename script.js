52f09d3f85b6f046db375ceb03e6b4bc673beedf
WolfM4ster <rezgui.abdoullah06@gmail.com>
2017-12-08 23:59:40 +0100

    add music, background, and moves

--- script.js
+++ script.js
@@ -9,6 +9,35 @@ function init() {
 function GameFramework() {
   let canvas, ctx, width, height;
   let hero;
+  let audio;
+
+  window.addEventListener('mousemove', onMouseUpdate, false);
+  window.addEventListener('keydown', handleKeydown, false);
+ 
+  function handleKeydown(e){
+    switch(e.keyCode) {
+      case 37:
+        //left arrow
+        hero.x -=10;
+        break;
+      
+      case 38:
+        //up arrow
+        hero.y -=10;
+        break;
+
+      case 39: 
+        //right arrow
+        hero.x +=10;
+        break;
+      
+      case 40:
+        //down arrow
+        hero.y +=10;
+      default:
+        break;
+    }
+  };
 
   function init() {
     canvas = document.querySelector("#myCanvas");
@@ -17,12 +46,18 @@ function GameFramework() {
     width = canvas.width;
     height = canvas.height;
 
+    playMusic();
+
+    initialiseHeroAndPosition();
+  
+    animate();
+  }
+
+  function initialiseHeroAndPosition() {
     let x = canvas.width / 2;
     let y = canvas.height - 100;
 
     hero = new Hero(x, y);
-  
-    animate();
   }
 
   function animate() {
@@ -30,9 +65,23 @@ function GameFramework() {
       
       hero.draw(ctx);
 
-      //requestAnimationFrame(animate);
+      requestAnimationFrame(animate);
   }
 
+  function onMouseUpdate(e) {
+    hero.x = e.pageX;
+    hero.y = e.pageY;
+  }
+
+  function playMusic() {
+    audio = new Audio('Undertale OST - Battle Against A True Hero Extended.mp3');
+    audio.play();
+  }
+
+  function stopMusic() {
+    audio.stop();
+  }
+  
   return {
     init:init
   }
@@ -56,23 +105,4 @@ class Hero {
     ctx.restore();
   }
 }
-
-
-
-/*function onMouseUpdate(e) {
-    cursorX = e.pageX;
-    cursorY = e.pageY;
-}*/
-
-  /*function triangle (x,y){
-
-    ctx.beginPath();
-    ctx.moveTo(x, y-30);
-    ctx.lineTo(x-50, y+30);
-    ctx.lineTo(x+50, y+30);
-    ctx.fill();
-    requestAnimationFrame( function(){
-        triangle(cursorX,cursorY);
-      })
-  }*/
 
