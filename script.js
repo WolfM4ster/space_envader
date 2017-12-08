dad3c26f68f6b093eaed27566115f0ebe1bdde99
WolfM4ster <rezgui.abdoullah06@gmail.com>
2017-12-08 23:04:21 +0100

    improve performance

--- script.js
+++ script.js
@@ -19,50 +19,33 @@ function init() {
 
 anime();
 
-
-
-
-
-
-
 }
 
 
 function anime(){
-    ctx.fillStyle="red";
-    ctx.fillRect(0,0,width,height); 
-    ctx.fillStyle="black";
-    triangle(cursorX,cursorY);
-    requestAnimationFrame(anime);
-}
-
-function fond(){
+    ctx.clearRect(0, 0, width, height);
     
+    triangle(cursorX,cursorY);
+
+    requestAnimationFrame(anime);
 }
 
 
 document.addEventListener('mousemove', onMouseUpdate, false);
 document.addEventListener('mouseenter', onMouseUpdate, false);
+
 function onMouseUpdate(e) {
     cursorX = e.pageX;
     cursorY = e.pageY;
 }
 
-
-
-
-
-
-
   function triangle (x,y){
-
+    ctx.save();
     ctx.beginPath();
     ctx.moveTo(x, y-30);
     ctx.lineTo(x-50, y+30);
     ctx.lineTo(x+50, y+30);
     ctx.fill();
-    requestAnimationFrame( function(){
-        triangle(cursorX,cursorY);
-      })
+    ctx.restore();
   }
 
