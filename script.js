9aa679d7f099ef39d78591ba23ba1d4447fa8d36
WolfM4ster <rezgui.abdoullah06@gmail.com>
2017-12-18 12:39:34 +0100

    features

--- script.js
+++ script.js
@@ -7,92 +7,112 @@ function init() {
 }
 
 function GameFramework() {
-  let canvas, ctx, width, height;
+  let canvas = document.querySelector("#myCanvas");
+  let ctx = canvas.getContext("2d");
+  let width, height;
   let hero;
-  let audio;
+  let musicChoised = "./Star Trek U.S.S. Enterprise Theme.mp3";
+  let imageHero = 'USS_Enterprise.png';
+  let music = new Music(musicChoised);
+  let background = new Background("./space.jpg");
 
   window.addEventListener('mousemove', onMouseUpdate, false);
   window.addEventListener('keydown', handleKeydown, false);
- 
-  function handleKeydown(e){
-    if(hero.isInsideZone(width, height)) {
-      switch(e.keyCode) {
-        case 37:
-          //left arrow
-          hero.x -=10;
-          break;
-        
-        case 38:
-          //up arrow
-          hero.y -=10;
-          break;
-  
-        case 39: 
-          //right arrow
-          hero.x +=10;
-          break;
-        
-        case 40:
-          //down arrow
-          hero.y +=10;
-        default:
-          break;
+
+  function handleKeydown(e) {
+    if (hero.isInsideZone(width, height)) {
+      switch (e.keyCode) {
+        default: break;
       }
     }
   };
 
   function init() {
-    canvas = document.querySelector("#myCanvas");
-    ctx = canvas.getContext("2d");
-
     width = canvas.width;
     height = canvas.height;
 
-    //playMusic();
-
+    music.play();
     initialiseHeroAndPosition();
-  
+
     animate();
   }
 
   function initialiseHeroAndPosition() {
     let x = canvas.width / 2;
     let y = canvas.height - 350;
-    
-    hero = new Hero(x, y, 67, 200);
+    hero = new Hero(imageHero, x, y, 67, 200);
   }
 
   function animate() {
-      ctx.clearRect(0, 0, width, height);
-      
-      hero.draw(ctx);
+    ctx.clearRect(0, 0, width, height);
+    hero.draw(ctx);
 
-      requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
+  }
+
+  function isInsideZone(e) {
+    return (
+      (e.pageX >= 0 && e.pageX <= width) &&
+      ((e.pageX + hero.width) >= 0 && (e.pageX + hero.width) <= width) &&
+
+      (e.pageY >= 0 && e.pageY <= height) &&
+      ((e.pageY + hero.height) >= 0 && (e.pageY + hero.height) <= height)
+    )
   }
 
   function onMouseUpdate(e) {
-    if(hero.isInsideZone(width, height)) {
+    if (hero.isInsideZone(width, height) && isInsideZone(e)) {
       hero.x = e.pageX;
       hero.y = e.pageY;
-    } 
-  }
-
-  function playMusic() {
-    audio = new Audio('C:/Users/Hichem/Music/zelda/Song of Unhealing - The legend of Zelda Majoras Mask.mp3');
-    audio.play();
-  }
-
-  function stopMusic() {
-    audio.stop();
+    }
   }
 
   return {
-    init:init
+    init: init
   }
 }
 
+function Music(musicChoised) {
+  let audio;
+
+  function play() {
+    audio = new Audio(musicChoised);
+    audio.play();
+
+    audio.addEventListener("ended", function () {
+      audio.play();
+    });
+  }
+
+  function stop() {
+    audio.stop();
+  }
+
+  function change(newMusic) {
+    musicChoised = newMusic;
+  }
+
+  return {
+    play: play,
+    change: change,
+    stop: stop
+  }
+}
+
+class Background {
+  constructor(imageSrc) {
+    this.imageSrc = imageSrc;
+  }
+
+  move() {
+
+  }
+
+}
+
 class Hero {
-  constructor(positionX, positionY, width, height) {
+  constructor(imageSrc, positionX, positionY, width, height) {
+    this.imageSrc = imageSrc;
     this.x = positionX;
     this.y = positionY;
     this.width = width;
@@ -102,21 +122,18 @@ class Hero {
   draw(ctx) {
     ctx.save();
     var img = new Image();
-    img.src = 'USS_Enterprise.png';
+    img.src = this.imageSrc;
     ctx.drawImage(img, this.x, this.y, this.width, this.height);
     ctx.restore();
-  };
+  }
 
   isInsideZone(width, height) {
-    if (
-        (this.x >= 0 && this.x <= width) &&
-        ((this.x + this.width) >= 0 && (this.x + this.width) <= width) &&
+    return (
+      (this.x >= 0 && this.x <= width) &&
+      ((this.x + this.width) >= 0 && (this.x + this.width) <= width) &&
 
-        (this.y >= 0 && this.y <= height) &&
-        ((this.y + this.height) >= 0 && (this.y + this.height) <= height)
-    ) {
-        return true;
-    }
-    return false;
-}
+      (this.y >= 0 && this.y <= height) &&
+      ((this.y + this.height) >= 0 && (this.y + this.height) <= height)
+    )
+  }
 }
